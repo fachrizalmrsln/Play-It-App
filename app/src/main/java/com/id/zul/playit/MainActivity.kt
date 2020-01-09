e03df3ca2c4327858c3e83ecc978fba0e16cebec
zul <fahrizalmursalin06@gmail.com>
2020-01-09 19:03:48 +0700

    Initial commit

---
+++ app/src/main/java/com/id/zul/playit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.id.zul.playit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
