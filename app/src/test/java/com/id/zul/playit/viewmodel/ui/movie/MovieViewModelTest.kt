6a2280bbd6882241795facc150d35fb600a92f81
zul <fahrizalmursalin06@gmail.com>
2020-01-14 15:34:34 +0700

    feat: creating unit test for checking getting data from API work properly.

--- app/src/test/java/com/id/zul/playit/viewmodel/ui/movie/MovieViewModelTest.kt
+++ app/src/test/java/com/id/zul/playit/viewmodel/ui/movie/MovieViewModelTest.kt
@@ -1,28 +1,51 @@
 package com.id.zul.playit.viewmodel.ui.movie
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.id.zul.playit.model.movie.Movie
 import com.id.zul.playit.repository.CatalogRepository
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class MovieViewModelTest {
 
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var viewModel: MovieViewModel
     private lateinit var catalogRepository: CatalogRepository
+
+    private lateinit var data: MutableLiveData<List<Movie>>
+    private lateinit var dummy: List<Movie>
+
+    private lateinit var observer: Observer<List<Movie>>
 
     @Before
     fun setUp() {
         catalogRepository = mock(CatalogRepository::class.java)
         viewModel = MovieViewModel(catalogRepository)
+
+        data = MutableLiveData()
+
+        observer = mock(Observer::class.java) as Observer<List<Movie>>
     }
 
     @Test
     fun getDummyMovie() {
-        val data = viewModel.dummyMovie
+        dummy = viewModel.dummyMovie
 
-        assertNotNull(data)
-        assertEquals(data.size, 20)
+        data.postValue(dummy)
+
+        `when`(catalogRepository.getNowPlayingMovie(1)).thenReturn(data)
+
+        viewModel.getNowPlaying(1).observeForever(observer)
+        verify(observer).onChanged(dummy)
+
+        assertEquals(dummy.size, 20)
     }
 }
--- app/src/test/java/com/id/zul/playit/viewmodel/ui/tv/TvViewModelTest.kt
+++ app/src/test/java/com/id/zul/playit/viewmodel/ui/tv/TvViewModelTest.kt
@@ -1,28 +1,51 @@
 package com.id.zul.playit.viewmodel.ui.tv
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.id.zul.playit.model.tv.Tv
 import com.id.zul.playit.repository.CatalogRepository
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class TvViewModelTest {
 
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var viewModel: TvViewModel
     private lateinit var catalogRepository: CatalogRepository
+
+    private lateinit var data: MutableLiveData<List<Tv>>
+    private lateinit var dummy: List<Tv>
+
+    private lateinit var observer: Observer<List<Tv>>
 
     @Before
     fun setUp() {
         catalogRepository = mock(CatalogRepository::class.java)
         viewModel = TvViewModel(catalogRepository)
+
+        data = MutableLiveData()
+
+        observer = mock(Observer::class.java) as Observer<List<Tv>>
     }
 
     @Test
-    fun getDummyTv() {
-        val data = viewModel.dummyTv
+    fun getDummyMovie() {
+        dummy = viewModel.dummyTv
 
-        assertNotNull(data)
-        assertEquals(data.size, 20)
+        data.postValue(dummy)
+
+        `when`(catalogRepository.getOnAirTv(1)).thenReturn(data)
+
+        viewModel.getOnAir(1).observeForever(observer)
+        verify(observer).onChanged(dummy)
+
+        assertEquals(dummy.size, 20)
     }
 }
