732a6175afc12917f7610a9034b90312d41dc1e4
zul <fahrizalmursalin06@gmail.com>
2020-01-18 15:33:42 +0700

    feat: creating local repository.

--- app/src/main/java/com/id/zul/playit/repository/source/local/LocalRepository.kt
+++ app/src/main/java/com/id/zul/playit/repository/source/local/LocalRepository.kt
@@ -1,8 +1,49 @@
 package com.id.zul.playit.repository.source.local
 
-class LocalRepository {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.id.zul.playit.data.local.DatabaseInstance
+import com.id.zul.playit.data.local.DatabaseObject
+import com.id.zul.playit.model.favorite.FavoriteEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class LocalRepository(context: Context) {
+
+    private val databaseObject: DatabaseObject
+
+    init {
+        val database = DatabaseInstance.getDatabase(context)
+        databaseObject = database.dataBaseObeject()
+    }
 
     companion object {
-        fun getInstance(): LocalRepository = LocalRepository()
+        fun getInstance(context: Context): LocalRepository = LocalRepository(context)
     }
+
+    fun insertFavorite(favoriteEntity: FavoriteEntity) {
+        GlobalScope.launch(Dispatchers.IO) {
+            databaseObject.insertFavorite(favoriteEntity)
+        }
+    }
+
+    fun deleteFavorite(favoriteEntity: FavoriteEntity) {
+        GlobalScope.launch(Dispatchers.IO) {
+            databaseObject.deleteFavorite(favoriteEntity)
+        }
+    }
+
+    fun getFavoriteMovie(): LiveData<List<FavoriteEntity>> {
+        return databaseObject.getAllFavoriteMovie()
+    }
+
+    fun getFavoriteTv(): LiveData<List<FavoriteEntity>> {
+        return databaseObject.getAllFavoriteTv()
+    }
+
+    fun getFavoriteById(id: Int): LiveData<FavoriteEntity> {
+        return databaseObject.getFavoriteById(id)
+    }
+
 }
